5c1777348cda075be11631a22fcabf57e45e6aba
Christian Fabók <christian.fabok@dci-student.org>
2025-01-24 15:56:17 +0100

    added

--- InventoryManagementSystem/main.py
+++ InventoryManagementSystem/main.py
@@ -1,5 +1,5 @@
 from inventory.product import Items
-from inventory.inventory_manager import get_total_inventory_value
+
 
 def total_prices():
     total_items_price = 0
@@ -7,7 +7,7 @@ def total_prices():
         price_to_use = item['purchasePrice'] if 'purchasePrice' in item else item['unitPrice']
         total_items_price += item['quantity'] * price_to_use
     
-    return total_items_price
+result = total_prices()
 
-total_items_value = get_total_inventory_value(Items)
-print(f'Total value of all available products: {total_items_value} EUR')
+#total_items_value = total_items_price
+print(f'Total value of all available products: {result} EUR')
